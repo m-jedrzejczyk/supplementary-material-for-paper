37dd7b3367a7eff3e32d5028b2934bc779b311b9
m-jedrzejczyk <82529996+m-jedrzejczyk@users.noreply.github.com>
2024-07-18 13:41:20 +0200

    Update NUTS a b c d calibrated.py

--- supplementary material/3 methods of UMIP treatments giving the same results/linear toy problem/NUTS a b c d calibrated.py
+++ supplementary material/3 methods of UMIP treatments giving the same results/linear toy problem/NUTS a b c d calibrated.py
@@ -3,13 +3,14 @@ import pymc as pm
 import arviz as az
 
 #observed data (experimental measurements)
-data = [163.1, 73.2, 115.7, 107.9]
+data = [163.6, 74.2, 116.7, 108.4]
 
 with pm.Model() as basic_model:
     # Priors for unknown model parameters
-    a = pm.Normal("a", mu=35, sigma=5)
-    b = pm.Normal("b", mu=15, sigma=3)
-    cd = pm.MvNormal("cd", mu=[8,4], cov=np.array([[1, 0.5], [0.5, 1]]))
+    a = pm.Normal("a", mu=35, sigma=7)
+    b = pm.Normal("b", mu=15, sigma=5)
+    cd = pm.MvNormal("cd", mu=[8,4], cov=np.array([[0.5**2, 0.5**3], [0.5**3, 0.5**2]]))
+    #mu = [3*a+2*b+c+d, a+b+2*c+d, (a+4*b+2*c+2*d), 2*a + b + c+d]
     mu = [3*a+2*b+cd[0]+cd[1], a+b+2*cd[0]+cd[1], (a+4*b+2*cd[0]+2*cd[1]), 2*a + b + cd[0]+cd[1]]
     # Distribution of observed data, including propagated uncertainties
     Y_obs = pm.Normal("Y_obs", mu=mu, sigma=0.1, observed=data)
@@ -22,7 +23,7 @@ print(az.summary(idata, kind="stats"))
 az.plot_trace(idata);
 
 # Saving the posterior data
-#az.to_netcdf(trace, '/home/jedrzejczykm/Desktop/my_papers/paper3/toy_models/trace_uncertainty_fully_neglected_NUTS_5000by4.nc')
+#az.to_netcdf(trace, '/home/jedrzejczykm/Desktop/my_papers/paper3/toy_models/trace_uncertainty_fully_neglected_NUTS_5000.nc')
 
 a = idata.posterior.a.data[0]
 b = idata.posterior.b.data[0]
