edffc56830035755fadf34e33e94c17a38d48c0a
m-jedrzejczyk <82529996+m-jedrzejczyk@users.noreply.github.com>
2024-07-18 13:43:19 +0200

    Update NUTS uncertainties propagated to observed data uncertainty.py

--- supplementary material/3 methods of UMIP treatments giving the same results/linear toy problem/NUTS uncertainties propagated to observed data uncertainty.py
+++ supplementary material/3 methods of UMIP treatments giving the same results/linear toy problem/NUTS uncertainties propagated to observed data uncertainty.py
@@ -3,9 +3,9 @@ import pymc as pm
 import arviz as az
 
 #observed data (experimental measurements)
-data = [163.1, 73.2, 115.7, 107.9]
+data = [163.6, 74.2, 116.7, 108.4]
 
-cd = np.random.multivariate_normal(mean=[8,4], cov=[[1, 0.5],[0.5,1]], size=1000000)
+cd = np.random.multivariate_normal(mean=[8,4], cov=[[0.5**2, 0.5**3], [0.5**3, 0.5**2]], size=1000000)
 measurement_error1=np.random.normal(0,0.1,1000000)
 measurement_error2=np.random.normal(0,0.1,1000000)
 measurement_error3=np.random.normal(0,0.1,1000000)
@@ -20,14 +20,14 @@ measurement_covariance = np.cov(stack_error_propagation)
 
 with pm.Model() as basic_model:
     # Priors for unknown model parameters
-    a = pm.Normal("a", mu=35, sigma=5)
-    b = pm.Normal("b", mu=15, sigma=3)
+    a = pm.Normal("a", mu=35, sigma=7)
+    b = pm.Normal("b", mu=15, sigma=5)
     # Mathematical models
     mu = [3*a+2*b+8+4, a+b+2*8+4, a+4*b+2*8+2*4, 2*a + b + 8+4]
     # Distribution of observed data, including propagated uncertainties
     Y_obs = pm.MvNormal("Y_obs", mu=mu, cov=measurement_covariance, observed=data)
     # Sampling
-    idata = pm.sample(tune=1000, draws=35000, cores=1, chains=1, step=pm.NUTS())
+    idata = pm.sample(tune=1000, draws=55000, cores=1, chains=1, step=pm.NUTS())
 
 # Printing posterior means and standard deviations, 3 digit precision
 print(az.summary(idata, kind="stats"))
@@ -41,9 +41,7 @@ b = idata.posterior.b.data[0]
 stackab = np.vstack((a, b))
 covab = np.cov(stackab)
 corrab = np.corrcoef(stackab)
-print("nuts_propagated cov:")
+print("nuts_posterior cov:")
 print(covab)
-print("nuts_propagated corr:")
+print("nuts_posterior corr:")
 print(corrab)
-
-
