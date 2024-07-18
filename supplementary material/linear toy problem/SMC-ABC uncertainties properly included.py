236391c96369c4fbc414f68d95a071aef9de4619
m-jedrzejczyk <82529996+m-jedrzejczyk@users.noreply.github.com>
2024-07-18 13:36:05 +0200

    Update SMC-ABC uncertainties properly included.py

--- supplementary material/linear toy problem/SMC-ABC uncertainties properly included.py
+++ supplementary material/linear toy problem/SMC-ABC uncertainties properly included.py
@@ -4,32 +4,27 @@ import numpy as np
 import pymc as pm
 
 # Assimilated data - experimental measurements
-data = [163.1, 73.2, 115.7, 107.9]
+data = [163.6, 74.2, 116.7, 108.4]
 # Random number generator, required for "Simulator"
 rng = np.random.default_rng()
 
 # Mathematical model of the assimilated data
 def normal_sim(rng, a, b, size=1):
-    # cd - the c and d variables modeled as correlated noise
-    cd=rng.multivariate_normal([8, 4], np.array([[1, 0.5], [0.5, 1]]), 1)
+    cd=rng.multivariate_normal([8, 4], np.array([[0.5**2, 0.5**3], [0.5**3, 0.5**2]]), 1)
     return [3*a+2*b+cd[0][0]+cd[0][1], a+b+2*cd[0][0]+cd[0][1], (a+4*b+2*cd[0][0]+2*cd[0][1]), 2*a + b + cd[0][0]+cd[0][1]]
 
 with pm.Model() as example:
     # Priors for unknown model parameters
-    a = pm.Normal("a", mu=35, sigma=5)
-    b = pm.Normal("b", mu=15, sigma=3)
+    a = pm.Normal("a", mu=35, sigma=7)
+    b = pm.Normal("b", mu=15, sigma=5)
     # epsilon is the uncertainty of experimental measurements of assimilated data. 
     # If the uncertainty of experimental measurements varies, the data and normal_sim results 
-    # would have to be rescaled. For example, if the third measurement (115.7) was taken with
-    # uncertainty of 1, while all others were taken with uncertainty of 0.1, the data
-    # would look as follows: data = [163.1, 73.2, 115.7/10, 107.9]
-    # and mathematical model would look as follows:
-    # 3*a+2*b+cd[0][0]+cd[0][1], a+b+2*cd[0][0]+cd[0][1], (a+4*b+2*cd[0][0]+2*cd[0][1])/10, 2*a + b + cd[0][0]+cd[0][1]
+    # would have to be rescaled. For example, if the third measurement (116.7) was taken with
+    # uncertainty of 1, while all others were taken with uncertainty of 0.1, the data and 
+    # mathematical model would look as follows: data = [163.6, 74.2, 116.7/10, 108.4],
+    # [3*a+2*b+cd[0][0]+cd[0][1], a+b+2*cd[0][0]+cd[0][1], (a+4*b+2*cd[0][0]+2*cd[0][1])/10, 2*a + b + cd[0][0]+cd[0][1]]
     s = pm.Simulator("s", normal_sim, params=(a, b), sum_stat="sort", epsilon=0.1, observed=data)
-    # Sampling step. 50000 is the number of samples to draw from the posterior and 
-    # the number of independent markov chains.
-    # To obtain a smoother posterior increase this number from 50000 to a higher one
-    idata = pm.sample_smc(50000, cores=1, chains=1, threshold=0.5)
+    idata = pm.sample_smc(95000, cores=1, chains=1, threshold=0.5)
     #sampling posterior values of data
     idata.extend(pm.sample_posterior_predictive(idata))
 
@@ -37,7 +32,7 @@ with pm.Model() as example:
 az.plot_trace(idata, kind="rank_vlines");
 print(az.summary(idata, kind="stats"))
 # Posterior of simulated quantitites
-#az.plot_trace(idata.posterior_predictive);
-#print(az.summary(idata.posterior_predictive, kind="stats"))
+az.plot_trace(idata.posterior_predictive);
+print(az.summary(idata.posterior_predictive, kind="stats"))
 # Saving the posterior data
-#az.to_netcdf(idata, '/home/jedrzejczykm/Desktop/paper/posterior_data_SMC_ABC.nc')
+#az.to_netcdf(idata, '/home/jedrzejczykm/Desktop/my_papers/paper3/toy_models/trace_uncertainty_included_SMC-ABC_200k.nc')
